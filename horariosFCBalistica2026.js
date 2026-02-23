08687acd0704d363652043bad747fda49931279c
jrozental-git <jrozental@insusep.edu.ar>
2026-02-23 16:36:52 -0300

    Update horariosFCBalistica2026.js with new course schedule

--- horariosFCBalistica2026.js
+++ horariosFCBalistica2026.js
@@ -1,5 +1,39 @@
-// Updated script
-// Sample content: Adjust the following string replacements as necessary
-let sampleContent = "MODULO 1 - Introduction to Programming\nMODULO 2 - Data Structures\nMODULO 3 - Algorithms";
-let updatedContent = sampleContent.replace(/MODULO (\d+) - /g, 'MODULO $1 - ');
-console.log(updatedContent);
+// horariosFCBalistica2026.js
+window.horariosCursos = {
+    "MODULO I - Balística Forense": {
+        horario: "Lunes de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        docente: ""
+    },
+    "MODULO II - Armas de Fuego y sus Mecanismos de Funcionamiento": {
+        horario: "Miércoles de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        docente: ""
+    },
+    "MODULO III - Análisis Legal y Pericial de Armas de Fuego y Material Balístico": {
+        horario: "Lunes de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        docente: ""
+    },
+ "MODULO IV - Balística Interior y Exterior": {
+        horario: "Miércoles de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        docente: ""
+    },
+ "MODULO V - Balística de efectos": {
+        horario: "Lunes de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        docente: ""
+    },
+ "MODULO VI - Inspección Ocular y Recolección de Indicios": {
+        horario: "Miércoles de 16 a 21 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        docente: ""
+    }
+};
