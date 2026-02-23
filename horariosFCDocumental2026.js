4c8e17c14adb3493dfc70da9853cfd968b026b39
jrozental-git <jrozental@insusep.edu.ar>
2026-02-23 18:27:56 -0300

    Create horariosFCDocumental2026.js

---
+++ horariosFCDocumental2026.js
@@ -0,0 +1,34 @@
+// horariosFCDocumental2026.js
+window.horariosCursos = {
+    "MODULO I - La Documentología": {
+        horario: "Martes de 16:00 a 21:20 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        docente: ""
+    },
+    "MODULO II - Estampado de sellos": {
+        horario: "Jueves de 16:00 a 21:20 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        docente: ""
+    },
+    "MODULO III - Identificación de Manuscritos": {
+        horario: "Martes de 16:00 a 21:20 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        docente: ""
+    },
+ "MODULO IV - Análisis de Documentos": {
+        horario: "Jueves de 16:00 a 21:20 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        docente: ""
+    },
+ "MODULO V - Identificación Marcaria": {
+        horario: "Martes de 16:00 a 21:20 hs.",
+        enlace: "Link al aula virtual",
+        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        docente: ""
+    }
+ 
+};
