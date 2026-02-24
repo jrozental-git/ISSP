625cf2c564a40311e9764a1f91bc3a50960bf1f3
jrozental-git <jrozental@insusep.edu.ar>
2026-02-24 09:37:46 -0300

    Update horariosFCBalistica2026.js

--- horariosFCBalistica2026.js
+++ horariosFCBalistica2026.js
@@ -2,37 +2,37 @@
 window.horariosCursos = {
     "MODULO I - Balística Forense": {
         horario: "Lunes de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
         docente: ""
     },
     "MODULO II - Armas de Fuego y sus Mecanismos de Funcionamiento": {
         horario: "Miércoles de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",    
         periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
         docente: ""
     },
     "MODULO III - Análisis Legal y Pericial de Armas de Fuego y Material Balístico": {
         horario: "Lunes de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
  "MODULO IV - Balística Interior y Exterior": {
         horario: "Miércoles de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
  "MODULO V - Balística de efectos": {
         horario: "Lunes de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
         docente: ""
     },
  "MODULO VI - Inspección Ocular y Recolección de Indicios": {
         horario: "Miércoles de 16 a 21 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
         docente: ""
     }
