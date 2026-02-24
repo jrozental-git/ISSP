015258427d7fcace730fdc8d8ba0cbaa6eb3e6d5
jrozental-git <jrozental@insusep.edu.ar>
2026-02-24 09:39:43 -0300

    Update horariosFCDocumental2026.js

--- horariosFCDocumental2026.js
+++ horariosFCDocumental2026.js
@@ -2,31 +2,32 @@
 window.horariosCursos = {
     "MODULO I - La Documentología": {
         horario: "Martes de 16:00 a 21:20 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
         periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
         docente: ""
     },
     "MODULO II - Estampado de sellos": {
         horario: "Jueves de 16:00 a 21:20 hs.",
-        enlace: "Link al aula virtual",
-        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+          enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
+          periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
         docente: ""
     },
     "MODULO III - Identificación de Manuscritos": {
         horario: "Martes de 16:00 a 21:20 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
+       
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
  "MODULO IV - Análisis de Documentos": {
         horario: "Jueves de 16:00 a 21:20 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
  "MODULO V - Identificación Marcaria": {
         horario: "Martes de 16:00 a 21:20 hs.",
-        enlace: "Link al aula virtual",
+        enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
         periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
         docente: ""
     }
