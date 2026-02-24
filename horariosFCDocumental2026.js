c91b971455089865605ba3d6ef41eea3a06ee28e
jrozental-git <jrozental@insusep.edu.ar>
2026-02-24 09:40:09 -0300

    Update horariosFCDocumental2026.js

--- horariosFCDocumental2026.js
+++ horariosFCDocumental2026.js
@@ -15,7 +15,6 @@ window.horariosCursos = {
     "MODULO III - Identificación de Manuscritos": {
         horario: "Martes de 16:00 a 21:20 hs.",
         enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTM2NzlkODktNWYwOS00NTY0LTk1ZGYtZjg5YmM3NGNlNmIw%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%22e210e659-1a74-45c4-88fb-5cd0ddd37a0f%22%7d",
-       
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
