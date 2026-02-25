6a38f1b41cf932d35ccff398eb46de29a410c0ad
jrozental-git <jrozental@insusep.edu.ar>
2026-02-25 16:26:52 -0300

    Update horariosFCBalistica2026.js

--- horariosFCBalistica2026.js
+++ horariosFCBalistica2026.js
@@ -24,7 +24,7 @@ window.horariosCursos = {
         periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
         docente: ""
     },
- "MODULO V - Balística de efectos": {
+ "MODULO V - Balística de Efectos": {
         horario: "Lunes de 16 a 21 hs.",
         enlace: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE5ZjFjYjUtZWM4Yy00ODIzLWEzMzctMzkwNGE3YTNmNDdk%40thread.v2/0?context=%7b%22Tid%22%3a%227f4ac155-2703-4e39-940d-ffda9afc707a%22%2c%22Oid%22%3a%223efbda4a-8453-4bcf-a2e1-39d8f85cdc71%22%7d",
         periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
