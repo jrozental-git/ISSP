7a789c7d14b4746c16ab8ff12b2bfd43fb9d886f
jrozental-git <jrozental@insusep.edu.ar>
2026-02-25 11:26:06 -0300

    Update horariosFCPapilocopia2026.js

--- horariosFCPapilocopia2026.js
+++ horariosFCPapilocopia2026.js
@@ -5,45 +5,45 @@ window.horariosCursos = {
     linkgeneral: linkgeneral,
 
     "MODULO I - Análisis Papiloscópico": {
-        horario: "Martes de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        periodo: "1er Bimestre - Junio",
         docente: ""
     },
     "MODULO II - Dactiloscopía": {
-        horario: "Jueves de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "1er Bimestre - 23 de Marzo al 15 de Mayo",
+        periodo: "1er Bimestre - Junio",
         docente: ""
     },
     "MODULO III - Sistemas de Clasificación Palametoscópica y Pelmatoscópica": {
-        horario: "Martes de 16:00 a 21:20 hs.",
+        horario: "Horario a definir.",
         enlace: linkgeneral,
-        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        periodo: "2do Bimestre",
         docente: ""
     },
     "MODULO IV - Identidad Física Humana": {
-        horario: "Jueves de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "2do Bimestre - 18 de Mayo al 3 de Julio",
+        periodo: "2do Bimestre",
         docente: ""
     },
     "MODULO V - Papiloscopía Aplicada": {
-        horario: "Martes de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        periodo: "3er Bimestre",
         docente: ""
     },
     "MODULO VI - Técnicas de Identificación Papiloscópica Alternativas": {
-        horario: "Martes de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        periodo: "3er Bimestre",
         docente: ""
     },
     "MODULO VII - Práctica Pericial Papiloscópica": {
-        horario: "Martes de 16:00 a 21:20 hs.",
+        horario: "Horario a definir",
         enlace: linkgeneral,
-        periodo: "3er Bimestre - 6 de Julio al 18 de Septiembre",
+        periodo: "3er Bimestre",
         docente: ""
     }
 };
