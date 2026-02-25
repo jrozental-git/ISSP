7e10d0c8d87dc8332ea80d90aee376e57d021fd8
jrozental-git <jrozental@insusep.edu.ar>
2026-02-25 16:49:02 -0300

    Update horariosFCPapilocopia2026.js

--- horariosFCPapilocopia2026.js
+++ horariosFCPapilocopia2026.js
@@ -2,7 +2,7 @@
 const linkgeneral = "http://www.webcampus.insusep.edu.ar";
 
 window.horariosCursos = {
-    linkgeneral: linkgeneral,
+
 
     "MÓDULO I - Análisis Papiloscópico": {
         horario: "Horario a definir",
